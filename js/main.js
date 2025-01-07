dbfff8822e5711c8b4a255c7f72c024823aa4d10
Yuri <urabuchko10@gmail.com>
2025-01-07 12:53:17 +0200

    ready fourth page

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ function addArrow(items) {
 
 const links = document.querySelectorAll(".click-link");
 addArrow(links);
+
+function swtchTabs(btns, tabs) {
+    btns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            btns.forEach((btn) => btn.classList.remove("is-active"));
+            btn.classList.add("is-active");
+
+            tabs.forEach((tab) => tab.classList.remove("is-show"));
+            tabs.forEach((tab) => {
+                if (tab.dataset.infotab === btn.dataset.btntab) {
+                    tab.classList.add("is-show");
+                }
+            });
+        });
+    });
+}
+
+const buttons = document.querySelectorAll(".tab-buttons button");
+const tabs = document.querySelectorAll(".tab-info article");
+swtchTabs(buttons, tabs);
